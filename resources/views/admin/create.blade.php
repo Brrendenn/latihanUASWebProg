c58cd91f0e0f519b272bbc0a6eae2e7a671b5218
Brandon's Laptop <BrandonsLaptop@Nicholass-MacBook-Air.local>
2026-01-19 11:12:59 +0700

    Belajar UAS

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -1,31 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="card mx-auto" style="max-width: 600px">
-        <div class="card-header">{{ __('text.book_admin') }}</div>
-        <div class="card-body">
-            <form action="{{ route('books.store') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                <div class="mb-3">
-                    <label class="form-label">{{ __('text.photo') }}</label>
-                    <input type="file" name="image" class="form-control" required>
-                </div>
+    <div class="container">
+        <div class="card mx-auto" style="max-width: 600px">
+            <div class="card-header">{{ __('text.book_admin') }}</div>
+            <div class="card-body">
+                <form action="{{ route('books.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="mb-3">
+                        <label class="form-label">{{ __('text.photo') }}</label>
+                        <input type="file" name="image" class="form-control">
+                    </div>
 
-                <div class="mb-3">
-                    <label class="form-label">{{ __('text.book_title') }}</label>
-                    <input type="text" name="title" class="form-control" placeholder="Ex: Belajar Laravel" required>
-                </div>
+                    <div class="mb-3">
+                        <label class="form-label">{{ __('text.book_title') }}</label>
+                        <input type="text" name="title" class="form-control" placeholder="Ex: Belajar Laravel" required>
+                    </div>
 
-                <div class="mb-3">
-                    <label class="form-label">{{ __('text.price') }}</label>
-                    <input type="number" name="price" class="form-control" placeholder="Ex: 100000" required>
-                </div>
+                    <div class="mb-3">
+                        <label class="form-label">{{ __('text.price') }}</label>
+                        <input type="number" name="price" class="form-control" placeholder="Ex: 100000" required>
+                    </div>
 
-                <button type="submit" class="btn btn-dark">{{ __('text.submit') }}</button>
-            </form>
+                    <button type="submit" class="btn btn-dark">{{ __('text.submit') }}</button>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 
 @endsection
